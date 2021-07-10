7ca2e27779d3e770ededc371ce1daa9c17b8c2d5
johnnyk-nn <44642@nelnet.net>
2021-07-10 13:06:22 -0500

    changes

--- script.js
+++ script.js
@@ -11,17 +11,20 @@ let numPlayer = 3;
 let PlayerStartArray = [...Array(numPlayer).keys()];
 let spaceTypeArray = ["Inn", "Souvenir", "Statue", "Visitor", "Field", "Cafe", "Mountain", "Mora", "Souvenir", "Statue", "Visitor", "Lake", "Mountain", "Cafe", "Inn", "Lake", "Statue", "Mora", "Field", "Mountain", "Visitor", "Statue", "Cafe", "Mountain", "Lake", "Souvenir", "Mora", "Inn", "Field", "Souvenir", "Visitor", "Mora", "Mountain", "Cafe", "Lake", "Field", "Statue", "Mora", "Visitor", "Lake", "Souvenir", "Inn", "Cafe", "Statue", "Visitor", "Souvenir", "Lake", "Mora", "Lake", "Visitor", "Mountain", "Field", "Lake", "Souvenir", "Inn"];
 let occupiedArray = new Array(52).fill(false);
-let initialTurnOrderArray = shuffle(PlayerStartArray);
-let charSelectionArray;
+let initialTurnOrderArray;
+let charSelectionArray; 
+let gameStartArray3P = [0,1,2,2,1,0];
+let gameStartArray4P = [0,1,2,3,3,2,1,0];
+let gameStartArray5P = [0,1,2,3,4,4,3,2,1,0];
 let game_end = false; 
 let players_at_inn = false;
 let firstTurn = true;
 let characterSelection = true;
-let player1 = {name:"", img: "", point:0, mora:0,souvenir:[],Cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:0,space:1, active:true};
-let player2 = {name:"", img: "", point:0, mora:0,souvenir:[],Cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:1,space:1, active:true};
-let player3 = {name:"", img: "", point:0, mora:0,souvenir:[],Cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:2,space:1, active:true};
-let player4 = {name:"", img: "", point:0, mora:0,souvenir:[],Cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:3,space:1, active:true};
-let player5 = {name:"", img: "", point:0, mora:0,souvenir:[],Cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:4,space:1, active:true};
+let player1 = {name:"", img: "", point:0, mora:0,souvenir:[],cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:0,space:1, active:true};
+let player2 = {name:"", img: "", point:0, mora:0,souvenir:[],cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:1,space:1, active:true};
+let player3 = {name:"", img: "", point:0, mora:0,souvenir:[],cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:2,space:1, active:true};
+let player4 = {name:"", img: "", point:0, mora:0,souvenir:[],cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:3,space:1, active:true};
+let player5 = {name:"", img: "", point:0, mora:0,souvenir:[],cafe_count:0,travelers:[], meals:[],field_art:0,mountain_art:0,lake_art:0, player_value:4,space:1, active:true};
 // let playersArray = [player1, player2, player3];
 let mealArray = [
   {
@@ -36,56 +39,14 @@ let mealArray = [
   },
 ]
 
-let charArray = [
-  {
-    "name": "Albedo",
-    "num": 2,
-    "img": "./img/thumb/Character_Albedo_Thumb.png",
-    "mora": 5,
-    "chosen": false
-  },
-  {
-    "name": "Mona",
-    "num": 1,
-    "img": "./img/thumb/Character_Mona_Thumb.png",
-    "mora": 5,
-    "chosen": false
-  },
-  {
-    "name": "Diluc",
-    "num": 4,
-    "img": "./img/thumb/Character_Diluc_Thumb.png",
-    "mora": 6,
-    "chosen": false
-  },
-  {
-    "name": "Klee",
-    "num": 3,
-    "img": "./img/thumb/Character_Klee_Thumb.png",
-    "mora": 2,
-    "chosen": false
-  },
-  {
-    "name": "Barbara",
-    "num": 5,
-    "img": "./img/thumb/Character_Barbara_Thumb.png",
-    "mora": 8,
-    "chosen": false
-  },
-  {
-    "name": "Aether",
-    "num": 6,
-    "img": "./img/thumb/Character_Barbara_Thumb.png",
-    "mora": 4,
-    "chosen": false
-  }
-]
+let charArray = [{ "name": "Albedo", "num": 2, "img": "./img/thumb/Character_Albedo_Thumb.png", "mora": 5, "chosen": false }, { "name": "Mona", "num": 1, "img": "./img/thumb/Character_Mona_Thumb.png", "mora": 5, "chosen": false },{ "name": "Diluc", "num": 4, "img": "./img/thumb/Character_Diluc_Thumb.png", "mora": 6, "chosen": false }, { "name": "Klee", "num": 3, "img": "./img/thumb/Character_Klee_Thumb.png", "mora": 2, "chosen": false }, { "name": "Barbara", "num": 5, "img": "./img/thumb/Character_Barbara_Thumb.png", "mora": 8, "chosen": false }, { "name": "Aether", "num": 6, "img": "./img/thumb/Character_Aether_Thumb.png", "mora": 4, "chosen": false }]
 
-
+// Function to get a random number
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+// Shuffle an array for randomization
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -104,6 +65,7 @@ function shuffle(array) {
   return array;
 }
 
+// Function to check if the space is a legal move and then resolve it
 function checkSpace(num){
     let potentialSpace = parseInt(num);
     console.log(potentialSpace);
@@ -211,6 +173,7 @@ function checkSpace(num){
       }
 }
 
+// Change the Inn value,, players are not allowed to cross the Inn value
 function newInnEvent(){
   switch(current_inn){
     case innstops[0]:
@@ -232,6 +195,7 @@ function newInnEvent(){
   return current_inn; 
 }
 
+// Function that fires off the space event after a legal move is made
 function checkSpaceType(event){
 console.log(event);
 switch(event){
@@ -270,24 +234,70 @@ switch(event){
   }
 }   
 
+// Function to set up the right player count
 function playerCountSelection(){
 
 }
 
+// Visually setup character details
+function setupCharacterDetails(){
+  // Setup Player 1 Data Display
+  $("#P1Name").text(player1["name"]);
+  $("#P1Points").text(player1["point"]);
+  $("#P1Mora").text(player1["mora"]);
+  $("#P1Souvenirs").text(player1["souvenir"]);
+  $("#P1Cafe").text(player1["cafe_count"]);
+  $("#P1Travelers").text(player1["travelers"]);
+  $("#P1Meals").text(player1["meals"]);
+  $("#P1Field").text(player1["field_art"]);
+  $("#P1Mountain").text(player1["mountain_art"]);
+  $("#P1Lake").text(player1["lake_art"]);
+  // Setup Player 2 Data Display
+  $("#P2Name").text(player2["name"]);
+  $("#P2Points").text(player2["point"]);
+  $("#P2Mora").text(player2["mora"]);
+  $("#P2Souvenirs").text(player2["souvenir"]);
+  $("#P2Cafe").text(player2["cafe_count"]);
+  $("#P2Travelers").text(player2["travelers"]);
+  $("#P2Meals").text(player2["meals"]);
+  $("#P2Field").text(player2["field_art"]);
+  $("#P2Mountain").text(player1["mountain_art"]);
+  $("#P2Lake").text(player1["lake_art"]);
+  // Setup Player 3 Data Display
+  $("#P3Name").text(player3["name"]);
+  $("#P3Points").text(player3["point"]);
+  $("#P3Mora").text(player3["mora"]);
+  $("#P3Souvenirs").text(player3["souvenir"]);
+  $("#P3Cafe").text(player3["cafe_count"]);
+  $("#P3Travelers").text(player3["travelers"]);
+  $("#P3Meals").text(player3["meals"]);
+  $("#P3Field").text(player3["field_art"]);
+  $("#P3Mountain").text(player3["mountain_art"]);
+  $("#P3Lake").text(player3["lake_art"]);
+}
+
+// Setup character selection
 function initializeCharSelection(){
-  charSelectionArray = shuffle(PlayerStartArray);
+  charSelectionArray = gameStartArray3P;
+  console.log(charSelectionArray);
   activeplayer["player_value"] = charSelectionArray[0];
 }
 
+// Once characters are selected, make board interactive and set up details.
 function initializeGameStart(){
-  if(charSelectionArray.length == 0){
+  if(charSelectionArray.length == 3){
     characterSelection = false;
-    // initialTurnOrderArray = shuffle(PlayerStartArray);
+    initialTurnOrderArray = charSelectionArray;
+    setupCharacterDetails();
     activeplayer["player_value"] = initialTurnOrderArray[0];
+    determineTurnOrder();
+  } else{
+ 
   }
-  console.log(initialTurnOrderArray);
+  console.log("charSelectionArray: " + charSelectionArray);
 }    
 
+// First turns once leaving the Inn
 function firstTurnOrder(){
   // console.log(initialTurnOrderArray);
   initialTurnOrderArray.shift();
@@ -299,6 +309,7 @@ function firstTurnOrder(){
   }
 }
 
+// Find the player with the smallest space value and make him/her/they the active player
 function determineTurnOrder(){
   switch(activeplayer["player_value"]){
     case 0:
@@ -343,6 +354,7 @@ function determineTurnOrder(){
   } 
 }
 
+// Visually display who should be active player
 function displayActivePlayer(activeplayer){
   let DP;
   switch(activeplayer["player_value"]) {
@@ -371,6 +383,7 @@ function innMealEvent(){
 
 }
 
+// Resolve character selection
 function char_selection(id){
   let validChoice = true;
   let char; 
